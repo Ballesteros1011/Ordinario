ea226bb995c14815219692878e3163fc17c67c2f
Ballesteros1011 <la.hilario24@info.uas.edu.mx>
2026-01-13 00:01:11 -0700

    Add PaginaDos component with motorcycle information

---
+++ Pagina2.jsx
@@ -0,0 +1,27 @@
+function PaginaDos() {
+  return (
+    <div>
+        <div className="card-header text-center bg-dark text-white p-4 rounded-top-lg">
+            <h1 className="fw-bolder m-0 display-4">MOTOCICLETAS</h1>
+            <p className="lead m-0">Agilidad, Accion, Velocidad y Practicidad</p>
+        </div>
+
+        <div className="mb-4 text-center">
+            <img 
+              src="https://imgs.search.brave.com/CMSP7F_tgtzzm9JnAFyQ79r3JDcnLUj9NrkhxhhPgR0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/ZnJlZS1waG90by9t/b3RvLXJpZGVyLW1h/a2luZy1zdHVudC1o/aXMtbW90b3JiaWtl/LWJpa2VyLWRvaW5n/LWRpZmZpY3VsdC1k/YW5nZXJvdXMtc3R1/bnRfNjU0MDgwLTEw/NTguanBnP3NlbXQ9/YWlzX2h5YnJpZCZ3/PTc0MCZxPTgw" 
+              className="img-fluid rounded shadow" 
+              alt="Motocicleta deportiva" 
+             />
+          </div>
+        <div>
+          <p className="lead text-center text-muted mb-4">
+          Las motocicletas son sinónimo de agilidad, eficiencia y una experiencia de conducción emocionante. Se destacan como una solución práctica y económica, especialmente en entornos urbanos.
+          Eficiencia y Economía: Las motos son considerablemente más eficientes en consumo de combustible que los carros, lo que se traduce en un menor gasto operativo. Su mantenimiento y costos de seguro suelen ser también más bajos.
+          Inigualable Agilidad en el Tráfico: Permiten sortear la congestión vehicular con facilidad, reducir drásticamente los tiempos de desplazamiento y aprovechar al máximo la infraestructura urbana.
+          Facilidad de Estacionamiento: Su tamaño compacto permite encontrar y utilizar espacios de estacionamiento mucho más pequeños y accesibles que los requeridos por un automóvil.
+          Experiencia de Conducción: Para muchos, la moto no es solo un medio de transporte, sino un estilo de vida. Ofrecen una conexión directa con el entorno y una sensación de libertad difícil de igualar en un carro.</p>             
+        </div>
+    </div>
+  );
+}
+export default PaginaDos;
