176ad4e3d4e7ff78e25d18f8d181fd2dda7280e0
Ballesteros1011 <la.hilario24@info.uas.edu.mx>
2026-01-12 23:58:49 -0700

    Inicio del sitio React

---
+++ Inicio.jsx
@@ -0,0 +1,26 @@
+function Inicio() { 
+  return (
+    <div>
+      <div className="card-header text-center bg-info text-white p-4 rounded-top">
+        <h1 className="fw-bolder m-0 display-4">COMPARACIONES PERRONAS</h1>
+        <p className="lead m-0">CARROS VS MOTOCICLETAS</p>
+      </div>
+      <div className="mb-4 text-center">
+          <img 
+            src="https://imgs.search.brave.com/inm3nAOVM8YuOATWUK-iBGuJo-0WHDX7aV3n6HRxtXY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/Y2Fycm9zYmxvZy5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MTUvMDcvY2Fycm8t/bW90by5qcGc"
+            className="img-fluid rounded shadow-sm border border-secondary" 
+            alt="Carro familiar" 
+          />
+        </div>
+      <div>
+        <h1 className="lead text-center text-muted mb-4">
+          Bienvenido al espacio donde la movilidad es la protagonista.
+          Explorar el mundo es una necesidad, y la elección de tu vehículo define tu viaje. ¿Buscas la seguridad inigualable, el confort y la capacidad de carga de un Carro para tus aventuras familiares? ¿O prefieres la agilidad, la eficiencia y la libertad que solo una Motocicleta puede ofrecerte para conquistar la ciudad?
+          Descubre las características que hacen único a cada medio de transporte y encuentra el compañero perfecto para tu ruta. Tu camino te espera.
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Inicio;
