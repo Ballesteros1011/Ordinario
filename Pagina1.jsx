c607eb1021465ccb70d1009479d65d532b98be96
Ballesteros1011 <la.hilario24@info.uas.edu.mx>
2026-01-13 00:00:43 -0700

    Create PaginaUno component for automobile information

---
+++ Pagina1.jsx
@@ -0,0 +1,29 @@
+function PaginaUno() {
+  return (
+    <div>
+      <div className="card-header text-center bg-primary text-white p-4 rounded-top">
+        <h1 className="fw-bolder m-0 display-4">AUTOMÓVILES (CARROS)</h1>
+        <p className="lead m-0">Seguridad, Confort y Versatilidad Familiar</p>
+      </div>
+      <div>
+        <div className="mb-4 text-center">
+          <img 
+            src="https://imgs.search.brave.com/TwVF4LSrzfs48sOcAFmtcZWSS-bllCdcPi42YMhlDYw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/Zm90b3MtZ3JhdGlz/L211bGhlci1qb3Zl/bS1hLWNvbXByYXIt/dW0tY2Fycm9fMTMw/My0xMzYyOC5qcGc_/c2VtdD1haXNfaHli/cmlkJnc9NzQwJnE9/ODA"
+            className="img-fluid rounded shadow-sm border border-secondary" 
+            alt="Carro familiar" 
+          />
+        </div>
+      </div>
+      <div>
+        <p className="lead text-center text-muted mb-4">Los carros ofrecen una combinación inigualable de seguridad, comodidad y versatilidad, siendo la opción predilecta para muchas personas y familias.
+        Mayor Seguridad: Gracias a su estructura de chasis, bolsas de aire (airbags) y avanzados sistemas de asistencia al conductor (como ABS y control de estabilidad), los carros brindan una protección significativamente superior en caso de colisión.
+        Confort y Capacidad: Proporcionan un refugio ante las inclemencias del tiempo (lluvia, frío, calor). Además, ofrecen espacio para múltiples pasajeros y un maletero amplio para transportar equipaje, compras o equipos deportivos.
+        Versatilidad para Todo Tipo de Viajes: Son ideales para viajes largos por carretera, trayectos con niños o mascotas, y para cuando se requiere transportar objetos voluminosos.
+        Privacidad: El interior cerrado del vehículo proporciona un espacio privado, lo que es especialmente valorado en el tráfico urbano o al hacer diligencias.</p>
+      </div>
+    </div>
+  );
+}
+
+
+export default PaginaUno;
